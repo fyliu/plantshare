37f12863f7efd6d51407faa902146746039d3383
Fang Yi Liu <fangyiliu@gmail.com>
2024-12-06 14:58:51 -0800

    feat: add models Commodity and Category
    Commodity has a Category

--- plant_share/users/models.py
+++ plant_share/users/models.py
@@ -1,10 +1,26 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from django_extensions.db.models import TimeStampedModel
+from django_extensions.db.models import TitleDescriptionModel
+from django_extensions.db.models import TitleSlugDescriptionModel
 
 
-class User(AbstractUser):
+class AbstractBaseModel(TimeStampedModel):
+    """
+    Base abstract model
+    """
+
+    class Meta:
+        abstract = True
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} {self.pk}>"
+
+
+class User(AbstractUser, AbstractBaseModel):
     """
     Default custom user model for Plant Share.
     If adding fields that need to be filled at user signup,
@@ -24,3 +40,17 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+
+class Category(AbstractBaseModel, TitleSlugDescriptionModel):
+    """
+    Category of commodity
+    """
+
+
+class Commodity(AbstractBaseModel, TitleDescriptionModel):
+    """
+    Commodity being traded
+    """
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
